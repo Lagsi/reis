fd974f006391f4f5078a9c92ed1731425d5f4001
Nima <nima97e@gmail.com>
2023-05-12 19:48:16 +0330

    add movie information

--- movies.ts
+++ movies.ts
@@ -1,6 +1,26 @@
 export const movies = [
   {
     title: "Night of Silence",
+    cast: ["Dilan Aksüt", "Sabri Tutal", "İlyas Salman", "Mayşeker Yücel"],
+    synopsis:
+      "After a wedding ceremony for an arranged marriage, a 14-year-old bride is led to a bridal chamber and given instructions in nuptial customs by her sister-in-law.",
+    imgUrl:
+      "https://media-cache.cinematerial.com/p/500x/itrb8giy/lal-gece-british-movie-poster.jpg",
+    year: 2013,
+    awards: ["62.Berlin International Film Festival (Berlinale): Crystal Bear", "17.Nurnberg TD Film Festival-Germany: (Best Film, Best Actor, Best Actress )", "38.Wurzburg Film Festival-Germany: Best Film", "38.Buster International Film Festival-Denmark: Special Jury Price"],
+  },
+  {
+    title: "Tales of Intransigence",
+    cast: ["Tuncel Kurtiz", "Sabri Tutal", "Aslı Sulan", "Kemal Gültekin"],
+    synopsis:
+      "The story of rivalry between two forms of winter transport: the horse-drawn sleigh which can take the short cut over the frozen lake and the red minibus which needs to be more devious to beat its rival. While the race goes on other tales of intransigence are told in this land of bards and minstrels.",
+    imgUrl:
+      "https://media-cache.cinematerial.com/p/500x/itrb8giy/lal-gece-british-movie-poster.jpg",
+    year: 2003,
+    awards: ["Oscar: Best Movie", "bafta: Best British Award", "Cannes: Ayo!"],
+  },
+  {
+    title: "Food for a Funeral",
     cast: ["Me", "Nima"],
     synopsis:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe aspernatur ipsum nihil fugiat quo rem doloremque repudiandae, architecto natus molestiae odit a omnis, aut error delectus, quas nemo commodi ab amet laborum eligendi! Mollitia dolor, quis necessitatibus cum saepe magnam. Illum, nemo quisquam ducimus qui sed reprehenderit? Officiis, sed magni.",
@@ -10,27 +30,7 @@ export const movies = [
     awards: ["Oscar: Best Movie", "bafta: Best British Award", "Cannes: Ayo!"],
   },
   {
-    title: "Just something2",
-    cast: ["Me", "Nima"],
-    synopsis:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe aspernatur ipsum nihil fugiat quo rem doloremque repudiandae, architecto natus molestiae odit a omnis, aut error delectus, quas nemo commodi ab amet laborum eligendi! Mollitia dolor, quis necessitatibus cum saepe magnam. Illum, nemo quisquam ducimus qui sed reprehenderit? Officiis, sed magni.",
-    imgUrl:
-      "https://media-cache.cinematerial.com/p/500x/itrb8giy/lal-gece-british-movie-poster.jpg",
-    year: 2001,
-    awards: ["Oscar: Best Movie", "bafta: Best British Award", "Cannes: Ayo!"],
-  },
-  {
-    title: "Just something3",
-    cast: ["Me", "Nima"],
-    synopsis:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe aspernatur ipsum nihil fugiat quo rem doloremque repudiandae, architecto natus molestiae odit a omnis, aut error delectus, quas nemo commodi ab amet laborum eligendi! Mollitia dolor, quis necessitatibus cum saepe magnam. Illum, nemo quisquam ducimus qui sed reprehenderit? Officiis, sed magni.",
-    imgUrl:
-      "https://media-cache.cinematerial.com/p/500x/itrb8giy/lal-gece-british-movie-poster.jpg",
-    year: 2001,
-    awards: ["Oscar: Best Movie", "bafta: Best British Award", "Cannes: Ayo!"],
-  },
-  {
-    title: "Just something4",
+    title: "Goodbye Tomorrow",
     cast: ["Me", "Nima"],
     synopsis:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe aspernatur ipsum nihil fugiat quo rem doloremque repudiandae, architecto natus molestiae odit a omnis, aut error delectus, quas nemo commodi ab amet laborum eligendi! Mollitia dolor, quis necessitatibus cum saepe magnam. Illum, nemo quisquam ducimus qui sed reprehenderit? Officiis, sed magni.",
