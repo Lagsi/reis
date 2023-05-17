a37a3f81e578c8b13bfe1be5ca77a4142d549211
Nima <nima97e@gmail.com>
2023-05-17 22:34:47 +0330

    add movieNav to photography page

--- app/photography/page.tsx
+++ app/photography/page.tsx
@@ -1,3 +1,16 @@
+import MoviesNav from "../components/moviesNav";
+
 export default function PhotographyPage() {
-  return <div className="mx-auto text-8xl text-center">Coming soon...</div>;
+  return (
+    <div className="h-full min-h-[95vh] md:flex gap-4 p-4 lg:p-0 leading-7">
+      <div className="w-1/4 hidden md:block border-r">
+        <MoviesNav />
+      </div>
+      <div className="flex justify-center items-center w-full">
+        <h2 className="text-center text-7xl">
+          Coming soon...
+        </h2>
+      </div>
+    </div>
+  )
 }
