9e1d4f1086fd93bd0b730169a9df21b70cd03a5c
Jeppe Hauman <jeppe_hauman@hotmail.com>
2023-05-17 12:24:01 +0200

    add force-static to all pages

--- app/awards/page.tsx
+++ app/awards/page.tsx
@@ -7,19 +7,21 @@ export default function AwardsPage() {
       <div className="w-1/4 hidden md:block border-r">
         <MoviesNav />
       </div>
-      <div className="grid grid-cols-3 gap-3 w-full max-w-7xl p-6">
-        {awards.map((award) => (
-          <div className="p-3 h-96 text-4xl flex items-center hover:flex-col hover:justify-around hover:items-start max-w-s hover:w-full hover:bg-black hover:text-white hover:text-lg hover:font-black group">
-            <div className="max-h-[70%]">
-              <h2 className="">{award.title}</h2>
+      <div className="w-full">
+        <div className="grid grid-cols-3 gap-3 w-full max-w-7xl p-6">
+          {awards.map((award) => (
+            <div className="p-3 h-96 text-4xl flex items-center hover:flex-col hover:justify-around hover:items-start max-w-s hover:w-full hover:bg-black hover:text-white hover:text-lg hover:font-black group">
+              <div className="max-h-[70%]">
+                <h2 className="">{award.title}</h2>
+              </div>
+              <ul className="hidden group-hover:block">
+                {award.award.map((award) => (
+                  <li className="text-5xl">{award}</li>
+                ))}
+              </ul>
             </div>
-            <ul className="hidden group-hover:block">
-              {award.award.map((award) => (
-                <li className="text-5xl">{award}</li>
-              ))}
-            </ul>
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
     </div>
   );
